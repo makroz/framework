7efca3ebea56b8331a64b4de2fc412d247f836e6
makroz <makroz@hotmail.com>
2017-05-05 23:57:24 -0400

    tranajando en el cerud formulario ya se puede cambiar de posicion y tamano

--- mk/js/mk.js
+++ mk/js/mk.js
@@ -460,4 +460,14 @@ function setNodosTree(nodo,data){
 
 }
 
+function sortByCol(arr, colIndex){
+    arr.sort(sortFunction)
+    function sortFunction(a, b) {
+        a = a[colIndex]
+        b = b[colIndex]
+        return (a === b) ? 0 : (a < b) ? -1 : 1
+    }
+    return arr;
+}
+
 
